d8019679435baaa7a3b62c43b1e2effe9f59f2d3
ShreyPaharia <shreypaharia@gmail.com>
2018-09-20 17:17:39 +0200

    Add files via upload

--- src/Player.java
+++ src/Player.java
@@ -21,16 +21,16 @@ public class Player {
         gameState.findPossibleMoves(nextStates);
         PLAYER = gameState.getNextPlayer() ;
         OPPONENT = (PLAYER == Constants.CELL_X) ? Constants.CELL_O : Constants.CELL_X;
-        System.err.println("Size of next State" + nextStates.size());
+        
         int value ;
         int bestValue = Integer.MIN_VALUE ;
         int bestNextStateInd = -1  ;
-        System.err.println("123");
+        
         if (nextStates.size() == 0) {
             // Must play "pass" move if there are no other moves possible.
             return new GameState(gameState, new Move());
         }
-        System.err.println("nextState Size" + nextStates.size());
+        
         /**
          * Here you should write your algorithms to get the best next move, i.e.
          * the best next state. This skeleton returns a random move instead.
@@ -38,7 +38,7 @@ public class Player {
         for(int i=0 ; i<nextStates.size() ; i++){
             System.err.println(i);
 //            value =1;
-            value = MiniMaxPruning(nextStates.elementAt(i),2,Integer.MIN_VALUE,Integer.MAX_VALUE,gameState.getNextPlayer()) ;
+            value = MiniMax(nextStates.elementAt(i),gameState.getNextPlayer()) ;
             if(value > bestValue){
                 bestValue = value ;
                 bestNextStateInd = i ;
@@ -83,99 +83,103 @@ public class Player {
         }
                 
     }
-    
-    public int MiniMaxPruning(GameState gameState,int depth, int alpha, int beta, int player){
-        
-        Vector<GameState> nextStates = new Vector<>();
-        gameState.findPossibleMoves(nextStates);
-        int bestPossible ;
-        int value ;
-        System.err.println("Inside MinMax Depth=   "+depth) ;
-        if(depth ==0 ||gameState.isEOG())
-            return Evaluation(gameState) ;
-        
-        else if(player == PLAYER){
-            bestPossible = Integer.MIN_VALUE ;
-            for(int i=0 ; i<nextStates.size() ; i++){
-                value = MiniMaxPruning(nextStates.elementAt(i),depth-1, alpha, beta, OPPONENT) ;
-                bestPossible = Math.max(bestPossible, value) ;
-                alpha = Math.max(bestPossible,alpha) ;
-                if(beta<=alpha) break ;
-            }
-            return bestPossible ;
-        }
-        
-        else if(player == OPPONENT){
-            bestPossible = Integer.MAX_VALUE ;
-            for(int i=0 ; i<nextStates.size() ; i++){
-                value = MiniMaxPruning(nextStates.elementAt(i),depth-1, alpha, beta,PLAYER) ;
-                bestPossible = Math.min(bestPossible, value) ;
-                beta = Math.min(bestPossible, beta) ;
-                if(beta<=alpha) break ;
-            }
-            return bestPossible ;
-        
-        }
-        else
-            return Integer.MIN_VALUE ;
-                
-    }
-    
-    public int Evaluation (GameState gameState){
+        public int Evaluation (GameState gameState){
 //        System.err.println("Player.Evaluation()");
+        if(!(gameState.isOWin()||gameState.isXWin())) return 0 ;
+        else if((gameState.isXWin() && PLAYER == Constants.CELL_X) || (gameState.isOWin() && PLAYER == Constants.CELL_O)) return 1 ;
+        else return -1 ;
         
         
-        return (getScore(gameState, PLAYER)-getScore(gameState, OPPONENT)) ;
+        
         
     }
-    public int getScore(GameState gameState , int player) {
-        
-        int score = 0 ;
-        boolean full;
-        
-        //check rows
-        for(int i=0 ; i<SIZE ;i++){
-            full = true;
-            for(int j=0 ; j<SIZE ; j++) {
-                if(gameState.at(i,j)!= player){
-                    full = false ;
-                    break ;
-                }
-            }
-            if(full) score++ ;
-        }
-        
-        //check columns
-        for(int i=0 ; i<SIZE ;i++){
-            full = true ;
-            for(int j=0 ; j<SIZE ; j++) {
-                if(gameState.at(j,i)!= player){
-                    full = false ;
-                    break ;
-                }
-            }
-            if(full) score++ ;
-        }
-        //check diagonal1
-        full = true ;
-        for(int i=0 ; i<SIZE ;i++){
-            if(gameState.at(i,i)!= player){
-                full = false ;
-                break ;
-            }
-        }
-        if(full) score++ ;
-        
-        //check diagonal2
-        full = true ;
-        for(int i=0 ; i<SIZE ;i++){
-            if(gameState.at(SIZE-i-1,i)!= player){
-                full = false ;
-                break ;
-            }
-        }
-        if(full) score++ ;
-        
-        return score ;
-    }
+    
+//    public int MiniMaxPruning(GameState gameState,int depth, int alpha, int beta, int player){
+//        
+//        Vector<GameState> nextStates = new Vector<>();
+//        gameState.findPossibleMoves(nextStates);
+//        int bestPossible ;
+//        int value ;
+//        System.err.println("Inside MinMax Depth=   "+depth) ;
+//        if(depth ==0 ||gameState.isEOG())
+//            return Evaluation(gameState) ;
+//        
+//        else if(player == PLAYER){
+//            bestPossible = Integer.MIN_VALUE ;
+//            for(int i=0 ; i<nextStates.size() ; i++){
+//                value = MiniMaxPruning(nextStates.elementAt(i),depth-1, alpha, beta, OPPONENT) ;
+//                bestPossible = Math.max(bestPossible, value) ;
+//                alpha = Math.max(bestPossible,alpha) ;
+//                if(beta<=alpha) break ;
+//            }
+//            return bestPossible ;
+//        }
+//        
+//        else if(player == OPPONENT){
+//            bestPossible = Integer.MAX_VALUE ;
+//            for(int i=0 ; i<nextStates.size() ; i++){
+//                value = MiniMaxPruning(nextStates.elementAt(i),depth-1, alpha, beta,PLAYER) ;
+//                bestPossible = Math.min(bestPossible, value) ;
+//                beta = Math.min(bestPossible, beta) ;
+//                if(beta<=alpha) break ;
+//            }
+//            return bestPossible ;
+//        
+//        }
+//        else
+//            return Integer.MIN_VALUE ;
+//                
+//    }
+    
+
+//    public int getScore(GameState gameState , int player) {
+//        
+//        int score = 0 ;
+//        boolean full;
+//        
+//        //check rows
+//        for(int i=0 ; i<SIZE ;i++){
+//            full = true;
+//            for(int j=0 ; j<SIZE ; j++) {
+//                if(gameState.at(i,j)!= player){
+//                    full = false ;
+//                    break ;
+//                }
+//            }
+//            if(full) score++ ;
+//        }
+//        
+//        //check columns
+//        for(int i=0 ; i<SIZE ;i++){
+//            full = true ;
+//            for(int j=0 ; j<SIZE ; j++) {
+//                if(gameState.at(j,i)!= player){
+//                    full = false ;
+//                    break ;
+//                }
+//            }
+//            if(full) score++ ;
+//        }
+//        //check diagonal1
+//        full = true ;
+//        for(int i=0 ; i<SIZE ;i++){
+//            if(gameState.at(i,i)!= player){
+//                full = false ;
+//                break ;
+//            }
+//        }
+//        if(full) score++ ;
+//        
+//        //check diagonal2
+//        full = true ;
+//        for(int i=0 ; i<SIZE ;i++){
+//            if(gameState.at(SIZE-i-1,i)!= player){
+//                full = false ;
+//                break ;
+//            }
+//        }
+//        if(full) score++ ;
+//        
+//        return score ;
+//    }
 }
